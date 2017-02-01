977c435c177716a965cc15c73a3d992ac45d3e89
Esten Leonardsen <estenleonardsen@gmail.com>
2017-02-01 16:50:33 +0100

    Bugfix

--- train_localization_model.py
+++ train_localization_model.py
@@ -24,15 +24,15 @@ def train_localization_model(recognition_cnn=None, image_shape=DEFAULT_IMAGE_SHA
 	features = recognition_cnn.extract_features(images, LAYER_NAME)
 	X, y = label_localization_data(features, Y)
 	X, y = shuffle_data(X, y)
-	X, y = balance_dataset(X, y)
+	X, y = balance_dataset(X, y, 2)
 	counts = [0, 0]
 	for i in y:
 		counts[np.argmax(i)] = counts[np.argmax(i)] + 1
-	print(str(counts))
+	print('Counts: ' + str(counts))
 	train_X, train_y, val_X, val_y = split_data(X, y)
 
 	cnn = SingleLayerCNN('Fishes_localization', (3, 3, 512), 2)
-	cnn.fit(X, y, X, y, epochs=10)
+	cnn.fit(train_X, train_y, val_X, val_y, epochs=10)
 
 if __name__ == '__main__':
 	train_localization_model(verbose=True)
