15b70870acd8f62d4ab44502b0dec5479852d650
Esten Leonardsen <estenleonardsen@gmail.com>
2017-02-01 16:53:22 +0100

    Bugfix

--- utils/data/balance_dataset.py
+++ utils/data/balance_dataset.py
@@ -3,13 +3,17 @@ from collections import Counter
 
 def balance_dataset(X, y, classes):
 	counts = Counter([np.argmax(val) for val in y])
+	print('Counts: ' + str(counts))
 	min_count = min(counts)
+	print('Min count: ' + str(min_count))
 
 	balanced_X = []
 	balanced_y = []
 	val_counts = np.zeros(classes)
+	print('Val counts: ' + str(val_counts))
 	for i in range(0, len(X)):
 		val = np.argmax(y[i])
+		print('Found val: ' + str(val))
 		if val_counts[val] < min_count:
 			balanced_X.append(X[i])
 			balanced_y.append(y[i])
