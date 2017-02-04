ea1b82157b782a254dcca04c1470f190fdb70a1b
Esten Leonardsen <estenleonardsen@gmail.com>
2017-02-04 17:31:38 +0100

    Bugfixes

--- train_localization_model.py
+++ train_localization_model.py
@@ -23,6 +23,8 @@ def train_localization_model(recognition_cnn=None, image_shape=DEFAULT_IMAGE_SHA
 	gridsize, images, Y = parse_localization_data(SRC_FOLDER, DATA_FILE, image_shape, verbose=verbose)
 	features = np.flipud(recognition_cnn.extract_features(images, LAYER_NAME))
 	X, y = label_localization_data(features, Y)
+	print('X.shape: ' + str(X.shape))
+	print('y.shape: ' + str(y.shape))
 	X, y = shuffle_data(X, y)
 	X, y = balance_dataset(X, y, 2)
 	train_X, train_y, val_X, val_y = split_data(X, y)
